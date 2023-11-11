aa740e1a51b323f6b18f5d333fcbe39624edba79
Douglas <107089633+douglasrosso@users.noreply.github.com>
2023-11-11 14:08:45 -0300

    fix

---
+++ src/Layout.java
@@ -0,0 +1,71 @@
+import java.awt.event.ActionEvent;
+import java.io.File;
+
+import javax.swing.AbstractAction;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+public class Layout extends JFrame {
+  private JLabel lblChooser;
+  private JFileChooser txfChooser;
+  private JButton btnImportar;
+  private JTextField txtCaminho;
+  private FileNameExtensionFilter fneFilter;
+
+  public Layout() {
+    setTitle("Leitor Java Itarde");
+    setResizable(false);
+    setSize(900, 500);
+    setLayout(null);
+    setLocationRelativeTo(null);
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    ComponentesLoyout();
+    setVisible(true);
+  }
+
+  public void ComponentesLoyout() {
+    txfChooser = new JFileChooser();
+    txfChooser.setBounds(20, 60, 300, 400);
+
+    lblChooser = new JLabel("Importe seu arquivo para armazenar no banco:");
+    lblChooser.setBounds(20, 20, 300, 25);
+    getContentPane().add(lblChooser);
+
+    fneFilter = new FileNameExtensionFilter("TEXT FILES", "txt", "text");
+    txfChooser.setFileFilter(fneFilter);
+
+    txtCaminho = new JTextField();
+    txtCaminho.setBounds(20, 60, 640, 25);
+    txtCaminho.setEditable(false);
+    getContentPane().add(txtCaminho);
+
+    btnImportar = new JButton(new AbstractAction("Importar o arquivo") {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        int retorno = -1;
+
+        while (retorno == -1 || retorno == JFileChooser.APPROVE_OPTION) {
+          retorno = txfChooser.showOpenDialog(null);
+          if (retorno != JFileChooser.APPROVE_OPTION) {
+            break;
+          }
+          File arquivoSelecionado = txfChooser.getSelectedFile();
+          if (!arquivoSelecionado.getName().endsWith(".txt")) {
+            JOptionPane.showMessageDialog(null, "O arquivo deve ser do tipo .txt");
+          } else {
+            txtCaminho.setText(txfChooser.getSelectedFile().getAbsolutePath());
+            break;
+          }
+        }
+      }
+    });
+    btnImportar.setBounds(665, 60, 200, 25);
+    getContentPane().add(btnImportar);
+  }
+
+}
