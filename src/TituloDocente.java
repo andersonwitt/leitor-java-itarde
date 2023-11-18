8bfec13dded3c95782f7b12ee41f767c5a7c0ee6
Anderson Witt <anderson.witt@worktrucksolutions.com>
2023-11-18 15:25:37 -0300

    wip

--- src/TituloDocente.java
+++ src/TituloDocente.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 
 public class TituloDocente {
-  public final static HashMap<String, String> tituloDocente = new HashMap<String, String>() {
+  public final static HashMap<String, String> items = new HashMap<String, String>() {
     {
       put("01", "Pós-Graduação");
       put("02", "Mestrado");
